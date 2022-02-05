a697796b8ec1c9fe89d1778434ce7175a1249a1d
yochidros <yoshiki.myzw@gmail.com>
2022-02-05 11:25:41 +0900

    add: spm

---
+++ Package.swift
@@ -0,0 +1,26 @@
+// swift-tools-version:5.5
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "GithubKMMModule",
+    products: [
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
+        .library(
+            name: "GithubKMMModule",
+            targets: ["GithubKMMModule"]),
+    ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        // .package(url: /* package url */, from: "1.0.0"),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        .binaryTarger(
+            name: "GithubKMMModule",
+                      path: "xcframework/GithubKMMModule.xcframework"
+        )
+    ]
+)
